e7ad66574c98e7aab175955592c14b4f5ce095ef
daninfpj <daninfpj@users.noreply.github.com>
2024-04-12 17:50:46 -0500

    fix: failing test

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -24,10 +24,11 @@ def test_join_fields_for_orden_indirecta(orden_indirecta):
     assert join_fields(orden_indirecta, ORDEN_FIELDNAMES) == joined
 
 
-def test_join_fields_for_cuenta(cuenta):
-    cuenta.cuenta = '646180157099999993'
+def test_join_fields_for_cuenta(cuenta_persona_fisica):
+    cuenta_persona_fisica.cuenta = '646180157099999993'
     joined = '||TAMIZI|646180157099999993|SAHE800416HDFABC01||'
     assert join_fields(cuenta_persona_fisica, CUENTA_FIELDNAMES) == joined
+
 
 def test_compute_signature(client, orden):
     firma = (
